8bf6e34f6358581757d751b0c1b2416190584296
kawahara-yuri <as.22.kawahara@gmail.com>
2018-10-03 18:17:59 +0900

    Edit user.js

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,7 +1,7 @@
 $(function() {
-  var search_name = $("#chat-group-form__field--right-name");
+  var search_name = $("#user-search-result");
 
-  function appenddata(user) {
+  function appendData(user) {
     var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${user.name}</p>
                   <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
@@ -9,7 +9,7 @@ $(function() {
     search_name.append(html);
   }
 
-  function appendNodata(name) {
+  function appendNoData(name) {
     var html = `<div class='chat-group-user'>${ name }</div>`;
     search_name.append(html);
   }
@@ -24,18 +24,40 @@ $(function() {
       dataType: 'json'
     })
     .done(function(users) {
-      $("#chat-group-form__field--right-name").empty();
+      $("#user-search-result").empty();
       if (users.length !== 0) {
         users.forEach(function(user){
-          appenddata(user);
+          appendData(user);
         });
       }
       else {
-        appendNodata("一致するユーザーはいないよ");
+        appendNoData("一致するユーザーはいないよ");
       }
     })
     .fail(function() {
       alert('ユーザー検索に失敗残念');
     })
   });
+
+
+  var member = $("#chat-group-form__field--right-name");
+
+  function appendDataRemove(name, id) {
+    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+                  <input name='group[user_ids][]' type='hidden' value='${id}'>
+                  <p class='chat-group-user__name'>${name}</p>
+                  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                </div>`;
+    member.append(html);
+  }
+
+  $(document).on("click", ".user-search-add", function(){
+    var user = $(this).data();
+    appendDataRemove(user["userName"], user["userId"]);
+    $(this).parent().remove();
+  })
+
+  $(document).on("click", ".user-search-remove", function(user){
+    $(this).parent().remove();
+  })
 });
